fbbda96cebae5516ab24f995dac2b897f952de73
gage <117294201+g-a-h@users.noreply.github.com>
2022-11-28 14:36:40 -0700

    add nohup log rotate + kill msg

--- btc-wallet-scraper/kill-scrape.sh
+++ btc-wallet-scraper/kill-scrape.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+# gh - 2022
+#######
 
+echo -e "killing scrape processes...\n" 
 ps -ef | grep scrape  | awk '{ print $2 }'  | xargs kill -9
+mv nohup.out scrape-$(date +%y%m%d).log 
