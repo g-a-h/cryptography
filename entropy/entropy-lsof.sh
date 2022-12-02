5242543aaa4ba6fb35d23f1f4c62ce2075d31739
gage <117294201+g-a-h@users.noreply.github.com>
2022-12-01 21:10:09 -0700

    Update entropy-lsof.sh

--- entropy/entropy-lsof.sh
+++ entropy/entropy-lsof.sh
@@ -1,22 +1,16 @@
+#!/usr/bin/env bash 
 # gh - 2022
 # entropy from lsof
 
-iter=0 ;
 declare -a ntrop=() ;
-while true ; do
-  (( iter++ ))
-  [[ $iter -ge 24 ]] && break || {
-  top=$(lsof  |  awk '{ print $6 }' | grep 0x  | hexdump | sum | awk '{ print $1 }' | cut -b1-4)
-  while true ; do
-    btm=$(od -A n -t d -N 1 /dev/urandom)
-    [[ $btm -eq 0 ]] && : || break
-  done
-  while true ; do
-    rnd=$(expr $top "/" $btm)
-    [[ $rnd -ge 2047 ]] && : || { 
+while : ; do
+  [[ ${#ntrop[@]} -ge $1 ]] && break || {
+  rnd=$(expr $(od -An -td -N1 /dev/urandom | hexdump | sum | sum | sum | awk '{ print $1 }') \
+  "/" \
+  $(od -An -td -N1 /dev/urandom | sed 's/ 0 / 1 /g' ))
+  [[ $rnd -ge 2047 ]] && : || {
       [[ ! " ${ntrop[*]} " =~ " ${rnd} " ]] && ntrop+=("${rnd}")
-    } 
-  done 
+    }
   }
 done
 echo ${ntrop[@]}
